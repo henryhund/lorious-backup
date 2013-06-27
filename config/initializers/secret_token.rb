bd7832c09d345bab26d0b74b234427ecbbde95d9
Henry Hund <henryhund@gmail.com>
2013-06-27 11:16:47 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lorious::Application.config.secret_token = '4e10eaea07051dcef0041f91489f766197de078f6ffa16e591f7c8865ee07bcadc887ba0393593109ee939f16677cdc6607dca6e73bc01c3696ee71cd4f058a7'
