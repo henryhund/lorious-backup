5896969af590468987163b86b20ae42ee83550d5
Henry Hund <henryhund@gmail.com>
2013-06-27 21:30:59 -0400

    tried to fix devise issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,13 +61,15 @@ Lorious::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'lorious.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
+
+  config.assets.initialize_on_precompile = false
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
