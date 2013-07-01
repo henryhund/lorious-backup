a8d2380ac625fbc6790bf5093425045810f17911
Henry Hund <henryhund@gmail.com>
2013-07-01 16:39:53 -0400

    Updating opentok

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -8,7 +8,7 @@ class ChatController < ApplicationController
   def new
     @opentok = OpenTok::OpenTokSDK.new ENV["OPENTOK_APIKEY"], ENV["OPENTOK_APISECRET"], :api_url => ENV["OPENTOK_URL"]
     session_properties = {OpenTok::SessionPropertyConstants::P2P_PREFERENCE => "enabled"}    # or disabled
-
+    @location = "localhost"
     @session_id = @opentok.create_session( @location, session_properties )
     redirect_to "/chat/#{@session_id}"
   end
@@ -17,7 +17,6 @@ class ChatController < ApplicationController
     @session_id = params[:session_id]
 
     @opentok = OpenTok::OpenTokSDK.new ENV["OPENTOK_APIKEY"], ENV["OPENTOK_APISECRET"], :api_url => ENV["OPENTOK_URL"]
-    @location = "localhost"
     
 
     @token = @opentok.generate_token :session_id => @session_id, :role => OpenTok::RoleConstants::PUBLISHER
