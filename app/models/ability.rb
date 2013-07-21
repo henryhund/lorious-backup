c32a6ba15c5d0f5618f822d4113ffd0e559f1459
Henry Hund <henryhund@gmail.com>
2013-07-21 14:21:06 -0400

    updated permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,17 +8,36 @@ class Ability
     else
         if Rails.env.production?
           can :create, Request
+         
           can :create, Profile
           can :manage, Profile, :user_id => user.id 
+          can :show, Profile
+          # cannot :index, Profile
+          # can :create, Request
+
+          
+          can :read, User
           can :create, User
           can :manage, User, :id => user.id
-          can :finish_registration, :users
-          can :edit_incomplete_registration, :users
+          cannot :expert, User
+          # cannot :index, User
+
+          # can :finish_registration, :users
+          # can :edit_incomplete_registration, :users
+          
           can :chat_prep, :chat
+
+          # can :finish_registration, :users
+          # can :edit_incomplete_registration, :users
+          
+          # can :chat_prep, :chat
           can :scheduled_chat, :chat
+
           can :chat_end, :chat
           can :test, :chat
           can :report_listener, :chat
+          
+          # can :show, Profile
         else
           # can :manage, :all
           can :create, Request
